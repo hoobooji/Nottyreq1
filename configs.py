40c2b4b3b29720761f8c5e56fb4755d429331d19
hoobooji <162422722+hoobooji@users.noreply.github.com>
2024-12-02 13:55:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,17 +2,17 @@ from os import path, getenv
 import os, time 
 
 class Config:
-    API_ID = int(getenv("API_ID", ""))
-    API_HASH = getenv("API_HASH", "")
-    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    API_ID = int(getenv("API_ID", "21406615"))
+    API_HASH = getenv("API_HASH", "d66b1900ea3a7438ee22dd389085949a")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7895883671:AAFSp7wa7-fMdoXzP38Hz8O724Pr3g191CE")
  
-    ADMIN = list(map(int, getenv("ADMIN", "").split()))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-    FORCE_SUB = int(getenv("FORCE_SUB", ""))
+    ADMIN = list(map(int, getenv("ADMIN", "7974532619").split()))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+    FORCE_SUB = int(getenv("FORCE_SUB", "0"))
 
     # database configs
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://tharkihoobooji:tharkihoobooji@cluster0.sje0k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "tharkihoobooji")
     
     #web response 
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
